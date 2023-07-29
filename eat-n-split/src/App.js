702cfcb2497f0074aad1f83a9864127f2f1af197
Abdul Shahid Mohammed <shahidabdul235@gmail.com>
2023-07-28 22:20:44 -0400

    July 28

---
+++ eat-n-split/src/App.js
@@ -0,0 +1,138 @@
+import { useState } from "react";
+
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
+
+function App() {
+  const [addFriend, setAddFriend] = useState(false);
+
+  function displayAddFriendForm() {
+    setAddFriend(!addFriend);
+  }
+  return (
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+
+        {addFriend && (
+          <>
+            <FormAddFriend />
+            <Button onClickHandle={displayAddFriendForm}>Close</Button>
+          </>
+        )}
+
+        {!addFriend && (
+          <Button onClickHandle={displayAddFriendForm}>Add Friend</Button>
+        )}
+      </div>
+      <FormSplitBill />
+    </div>
+  );
+}
+
+function FriendsList() {
+  return (
+    <ul>
+      {initialFriends.map((friend) => (
+        <Friend friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+function Friend({ friend }) {
+  return (
+    <li>
+      <img src={friend.image} alt={friend.name} />
+      <h3>{friend.name}</h3>
+
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance === 0 && (
+        <p className="green">You and {friend.name} are even</p>
+      )}
+
+      <Button>Select</Button>
+    </li>
+  );
+}
+
+function Button({ onClickHandle, children }) {
+  return (
+    <button className="button" onClick={onClickHandle}>
+      {children}
+    </button>
+  );
+}
+
+function FormAddFriend() {
+  const [friendName, setFriendName] = useState(null);
+  // const [imageURL, setImageURL] = useState(null);
+  return (
+    <form className="form-add-friend">
+      <label>🧑‍🤝‍🧑Friend name</label>
+      <input
+        type="text"
+        value={friendName}
+        onChange={(e) => setFriendName(e.target.value)}
+      ></input>
+
+      <label>🖼️ Image URL</label>
+      <input type="text"></input>
+
+      <Button>Add</Button>
+    </form>
+  );
+}
+
+function FormSplitBill() {
+  return (
+    <form className="form-split-bill">
+      <h2>Split a bill with Friend</h2>
+
+      <label>💰 Bill Value</label>
+      <input type="text"></input>
+
+      <label>🕴️ Your expense</label>
+      <input type="text"></input>
+
+      <label>🧑‍🤝‍🧑Friend's expenses</label>
+      <input type="text" disabled></input>
+
+      <label>🤑 Who is paying the bill?</label>
+      <select>
+        <option value="You">You</option>
+        <option value="friend">Your Friend</option>
+      </select>
+
+      <Button>Split Bill</Button>
+    </form>
+  );
+}
+export default App;
