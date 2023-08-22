ab59df49b660e9dc6b16b94e96c9a438adccc3c0
Abdul Shahid Mohammed <shahidabdul235@gmail.com>
2023-08-21 22:14:25 -0400

    Aug 21 Added changes for city and contry lists, Form

--- worldwise/src/components/Map.jsx
+++ worldwise/src/components/Map.jsx
@@ -1,7 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
 
 function Map() {
-  return <div className={styles.mapContainer}>Map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer} onClick={() => navigate("form")}>
+      <h1>Map</h1>
+      <h1>
+        {" "}
+        position: {lat}, {lng}
+      </h1>
+    </div>
+  );
 }
 
 export default Map;
--- worldwise/src/components/SideBar.jsx
+++ worldwise/src/components/SideBar.jsx
@@ -2,12 +2,14 @@ import AppNav from "./AppNav";
 import Logo from "./Logo";
 import styles from "./Sidebar.module.css";
 
+import { Outlet } from "react-router-dom";
+
 function SideBar() {
   return (
     <div className={styles.sidebar}>
       <Logo />
       <AppNav />
-      <p>List of cities</p>
+      <Outlet />
       <footer className={styles.footer}>
         <p className={styles.copyright}>
           &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
